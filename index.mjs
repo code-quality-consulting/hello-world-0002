cd0de382c07f5b6eac65aba4fd4db93beb94a481
Mike Ricos <mike.ricos@gmail.com>
2018-04-09 13:30:14 -0500

    Simplifies conditional logic for Spanish.

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 export function makeGreeting(name = "world", language = "English") {
     if (language === "Spanish") {
-        if (name === "world" || name === "") {
+        if (name === "") {
             const world = "mundo";
             return "\u00A1Hola" + " " + world + "!";
         }
