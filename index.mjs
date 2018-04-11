2b1e11e0b1f0d76ffb190c13f7fbb8b46091e861
Ben Pardo <visaudi@gmail.com>
2018-04-11 13:44:40 -0500

    Substitutes 'world' via global dictionary array.

--- index.mjs
+++ index.mjs
@@ -1,15 +1,30 @@
+const dictionary = [
+    {
+        language: "English",
+        world: "world"
+    },
+    {
+        language: "Spanish",
+        world: "mundo"
+    },
+    {
+        language: "Russian",
+        world: "мир"
+    }
+];
+
 export function makeGreeter(language) {
     if (language === "Russian") {
-        return function (name = "мир") {
+        return function (name = dictionary[2].world) {
             return "Привет " + name + "!";
         };
     }
     if (language === "Spanish") {
-        return function (name = "mundo") {
+        return function (name = dictionary[1].world) {
             return "\u00A1Hola" + " " + name + "!";
         };
     }
-    return function (name = "world") {
+    return function (name = dictionary[0].world) {
         return "Hello " + name + "!";
     };
 }
