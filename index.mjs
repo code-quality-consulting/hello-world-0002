7a504dc09458ac9795da5ee26769157bd1ade1f0
Ben Pardo <visaudi@gmail.com>
2018-04-12 11:32:20 -0500

    Categorizes vocabulary parts by language.

--- index.mjs
+++ index.mjs
@@ -4,18 +4,14 @@ const worldInDifferentLanguages = {
     "Spanish": "mundo"
 };
 
+const helloInDifferentLanguages = {
+    "English": "Hello",
+    "Russian": "Привет",
+    "Spanish": "¡Hola"
+};
+
 export function makeGreeter(language) {
-    if (language === "Russian") {
-        return function (name = worldInDifferentLanguages.Russian) {
-            return "Привет " + name + "!";
-        };
-    }
-    if (language === "Spanish") {
-        return function (name = worldInDifferentLanguages.Spanish) {
-            return "\u00A1Hola" + " " + name + "!";
-        };
-    }
-    return function (name = worldInDifferentLanguages.English) {
-        return "Hello " + name + "!";
+    return function (name = worldInDifferentLanguages[language]) {
+        return helloInDifferentLanguages[language] + " " + name + "!";
     };
 }
